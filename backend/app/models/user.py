370c23e7889dbf3614fee181ced18696d22d37a2
AbhishekTHEdevonant <abhishekliteracyindia@gmail.com>
2026-09-05 11:41:57 -0700

    Set User.__tablename__ to users exactly as requested

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,11 +1,9 @@
 from sqlalchemy import Column, String
-from sqlalchemy.orm import declared_attr
 from app.db.base_class import Base
 
 class User(Base):
-    @declared_attr
-    def __tablename__(cls) -> str:
-        return "users"
+    __tablename__ = "users"
+
     id = Column(String, primary_key=True, index=True)
     username = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
